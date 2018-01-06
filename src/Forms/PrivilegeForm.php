c92448c41f8b4151bcc88b8e320ac2f392a89763
Radek Fryšták <geniv.radek@gmail.com>
2018-01-06 15:40:01 +0100

    remove default callback

--- src/Forms/PrivilegeForm.php
+++ src/Forms/PrivilegeForm.php
@@ -42,20 +42,6 @@ class PrivilegeForm extends Control
         $this->translator = $translator;
 
         $this->templatePath = __DIR__ . '/PrivilegeForm.latte';  // default path
-
-        // default onSuccess
-        if (!$this->onSuccess) {
-            $this->onSuccess[] = function () {
-                $this->redirect('this');
-            };
-        }
-
-        // default onError
-        if (!$this->onError) {
-            $this->onError[] = function () {
-                $this->redirect('this');
-            };
-        }
     }
 
 
